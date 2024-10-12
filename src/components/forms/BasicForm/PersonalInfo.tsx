066f08a907fe63cb88108582e997dde3326bd950
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-10-12 23:59:21 +0500

    Personal info form almost done

--- src/components/forms/BasicForm/PersonalInfo.tsx
+++ src/components/forms/BasicForm/PersonalInfo.tsx
@@ -1,7 +1,42 @@
+import { BasicPersonalInfoType } from '@/lib/types/BasicFormType/basic-form.type';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import {} from '';
+import ErrorMessage from '@/components/shared/ErrorMessage';
 
 function PersonalInfo() {
-  return <div>PersonalInfo</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BasicPersonalInfoType>({
+    // resolver:zodReso
+  });
+
+  const formSubmit = (formdata: BasicPersonalInfoType) => {
+    console.log(formdata);
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Personal Info</h1>
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" {...register('name')} />
+          {/* <ErrorMessage>{errors.name?.message}</ErrorMessage> */}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" {...register('email')} />
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <input type="number" id="age" {...register('age')} />
+        </div>
+        <button className="inline-block ml-auto">Next</button>
+      </form>
+    </div>
+  );
 }
 
 export default PersonalInfo;
---
+++ src/components/shared/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ErrorMessage({ message }: { message: string }) {
+  if (!message) return;
+  return <div className="text-red-500 text-sm">{message}</div>;
+}
+
+export default ErrorMessage;
